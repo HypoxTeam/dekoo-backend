1e35581cff6e2016771a6a44c4525815b9a9bf28
Diego Cardenas <49890496+DiegoNighty@users.noreply.github.com>
2021-12-28 18:27:45 -0600

    added windows64 support to compile protos script

--- script/compile-protos.sh
+++ script/compile-protos.sh
@@ -8,8 +8,17 @@ PROTO_DEST=./src/proto
 mkdir -p ${PROTO_DEST}
 
 # TypeScript code generation
-protoc \
-    --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
-    --ts_out=${PROTO_DEST} \
-    -I ./proto \
-    proto/*.proto
+if [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+  echo "Running script as Windows system"
+  protoc \
+      --plugin=protoc-gen-ts.cmd=./node_modules/.bin/protoc-gen-ts \
+      --ts_out=${PROTO_DEST} \
+      -I ./proto \
+      proto/*.proto
+else
+  protoc \
+      --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
+      --ts_out=${PROTO_DEST} \
+      -I ./proto \
+      proto/*.proto
+fi
